5f5887269edf99d85c1f43556aa733bee7a9a748
Huda Sahaf <hudasahaf27@gmail.com>
2023-04-03 14:18:58 -0400

    Download and save raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,45 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from https://github.com/the-pudding/data
+# Author: Huda Sahaf 
+# Data: 31 March 2023 
+# Contact: huda.sahaf@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: -
+# Any other information needed? -
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(readr)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(here)
+library(dplyr)
 
+#There are three data sets used for this analysis. All three are data sets used
+# the Puddings Publications, specifically, Beauty Brawl and Colorism in High Fashion.
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+fashion_tones <-
+  read.csv(
+    file = "https://raw.githubusercontent.com/the-pudding/data/master/vogue/faces.csv", 
+    skip = 0 
+  )
 
+fashion_frequency <-
+  read.csv(
+    file = "https://raw.githubusercontent.com/the-pudding/data/master/vogue/models.csv", 
+    skip = 0 
+  )
 
+makeup_shades <-
+  read.csv(
+    file = "https://raw.githubusercontent.com/the-pudding/data/master/makeup-shades/shades.csv", 
+    skip = 0 
+  )
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
+write_csv(fashion_tones, here::here("inputs/data/fashion_tones.csv")) 
+
+write_csv(fashion_frequency, here::here("inputs/data/fashion_frequency.csv")) 
+
+write_csv(makeup_shades, here::here("inputs/data/makeup_shades.csv")) 
          
